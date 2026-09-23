b6dbca7b47b24ee4151bc8914223181b8a567e5e
Shahriar Jaman <141053387+pabel172@users.noreply.github.com>
2026-09-23 11:48:40 +0600

    Add files via upload

---
+++ CircleFTP/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "CircleFTP provider for CloudStream"
+    authors = listOf("Shahriar Jaman")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=new.circleftp.net&sz=128"
+    language = "en"
+}
